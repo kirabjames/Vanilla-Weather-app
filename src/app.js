1a6ba9b5527c9f02230249291f8913707d970267
kirabjames <128862940+kirabjames@users.noreply.github.com>
2023-05-21 20:15:54 -0700

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apikey = "18850dff4bd6a23b322d88b9429e7a82";
+let apiurl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=$8850dff4bd6a23b322d88b9429e7a82&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
